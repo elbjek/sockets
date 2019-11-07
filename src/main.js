4999a3fe5e13877290837567099e18dac5cfef05
Lena Bjekovic <lb@target-traffic.net>
2019-11-07 10:57:38 +0100

    Repo setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import socketio from 'socket.io';
+import VueSocketIO from 'vue-socket.io';
+
+export const SocketInstance = socketio('http://localhost:4113');
+
+Vue.use(VueSocketIO, SocketInstance)
+
+
 Vue.config.productionTip = false
 
 new Vue({
